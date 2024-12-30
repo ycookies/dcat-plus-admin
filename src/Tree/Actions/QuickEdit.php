e97d163b64ecc63ef79e2b8515b490eae3806898
yangG <3664839@qq.com>
2024-12-30 20:45:15 +0800

    up Home page

--- src/Tree/Actions/QuickEdit.php
+++ src/Tree/Actions/QuickEdit.php
@@ -17,9 +17,9 @@ class QuickEdit extends RowAction
             ->click('.tree-quick-edit')
             ->success('Dcat.reload()')
             ->dimensions($width, $height);
-
+        $tips_title = trans('admin.quick_edit');
         return <<<HTML
-<a href="javascript:void(0);" data-url="{$this->resource()}/{$this->getKey()}/edit" class="tree-quick-edit"><i class="feather icon-edit"></i>&nbsp;</a>
+<a href="javascript:void(0);" data-url="{$this->resource()}/{$this->getKey()}/edit" class="tree-quick-edit"><i class="feather icon-edit tips" data-title="{$tips_title}"></i>&nbsp;</a>
 HTML;
     }
 }
